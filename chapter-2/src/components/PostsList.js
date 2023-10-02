f1fa5e56bc023846cff06531b9029cced241e97b
purwar1997 <shubhampurwar511@gmail.com>
2023-10-02 20:31:45 +0530

    Complete chapter-2

--- chapter-2/src/components/PostsList.js
+++ chapter-2/src/components/PostsList.js
@@ -15,7 +15,7 @@ const PostsList = () => {
     <section className='mt-10'>
       <h2 className='text-2xl'>All posts</h2>
 
-      <div className='mt-5 space-y-4'>
+      <div className='mt-6 space-y-4'>
         {orderedPosts.map(post => (
           <article className='border border-gray-500 px-5 py-4 rounded-xl' key={post.id}>
             <h3 className='text-xl'>{post.title}</h3>
