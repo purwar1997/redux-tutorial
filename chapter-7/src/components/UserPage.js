4a4085f60c1d2707956c8945036ddc71835b1a35
purwar1997 <shubhampurwar511@gmail.com>
2024-02-04 22:04:30 +0530

    Update data fetching logic for UserPage

--- chapter-7/src/components/UserPage.js
+++ chapter-7/src/components/UserPage.js
@@ -1,20 +1,37 @@
 import { useParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { getPostsByUser } from '../app/slices/postsSlice';
+import { createSelector } from '@reduxjs/toolkit';
+import { useGetPostsQuery } from '../app/slices/apiSlice';
 import { getUserById } from '../app/slices/usersSlice';
+import Spinner from './Spinner';
 
 const UserPage = () => {
   const { userId } = useParams();
 
   const user = useSelector(state => getUserById(state, userId));
-  const posts = useSelector(state => getPostsByUser(state, userId));
+
+  const getPostsByUser = createSelector(
+    [result => result.data, (result, userId) => userId],
+    (posts, userId) => posts.filter(post => post.user === userId)
+  );
+
+  const { postsByUser, isFetching } = useGetPostsQuery(undefined, {
+    selectFromResult: result => ({
+      ...result,
+      postsByUser: getPostsByUser(result, userId),
+    }),
+  });
+
+  if (isFetching) {
+    return <Spinner text='Loading posts...' />;
+  }
 
   return (
     <section>
       <h2 className='text-2xl'>{user.name}</h2>
 
       <ul className='mt-8 space-y-3 list-disc'>
-        {posts.map(post => (
+        {postsByUser.map(post => (
           <li key={post.id}>
             <Link
               className='text-purple-600 underline-offset-2 hover:underline'
